528177987cb1e5bcc3a4eae7fec75bc97941a3bf
Sonicfury <theo.lambrt@gmail.com>
2022-09-06 23:10:23 +0200

    fix: megaMenu x overflow

--- src/components/MegaMenu.svelte
+++ src/components/MegaMenu.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import CTA from './CTA.svelte'
     import logo from '/images/logo.webp'
-    import logoAlt from '/images/logo-alt.webp'
+    import logoAlt from '/images/logo-alt-vert.webp'
     import {fly} from 'svelte/transition'
     import {sineOut} from "svelte/easing"
 
@@ -16,10 +16,10 @@
 
 {#if isMenuEnabled}
     <div transition:fly="{{ y: -200, easing: sineOut, duration: 500 }}" on:click={handleClick}
-         class="fixed top-0 left-0 h-screen w-screen flex gap-4 bg-gunmetal-gray-dark">
-        <div class="hidden md:flex justify-center items-center grow"><img src={logoAlt} alt="logo" class="h-40 w-40">
+         class="fixed top-0 left-0 h-screen max-w-screen w-full flex bg-gunmetal-gray-dark">
+        <div class="hidden md:flex justify-center items-center w-full"><img src={logoAlt} alt="logo" class="h-96 w-96">
         </div>
-        <div class="flex justify-center items-center grow">
+        <div class="flex justify-center items-center w-full">
             <ul class="flex flex-col gap-4 items-center justify-center">
                 <li>
                     <CTA link="https://core.undergroundsociety.xyz">
